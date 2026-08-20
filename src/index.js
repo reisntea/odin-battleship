431d7ce7479c9aafff0f455b5f2a53824586a350
Brian C <283823263+reisntea@users.noreply.github.com>
2026-08-20 16:39:47 -0400

    Add set up ui to page.
    Added html and css to display the set up phase of battleship. Also made
    added some starting code for the set up phase in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import "./styles.css";
+
+// Divided into two phases in two functions, set-up and play game.
+function ScreenController() {
+  // Vars for setup
+  const setUpGrid = document.getElementById("set-up-grid");
+  let currShip = 0; // Going by index of ships in gameboard so index 0 is carrier
+  let currHorizontal = true;
+
+  function setUp() {
+    const shipPlacements = []; // Contains the placement of all the ships. Also goes by index in gameboard.
+
+    const createGrid = () => {
+      function createBox(row, x, y) {
+        const box = document.createElement("div");
+        box.classList.add("boxy");
+        box.setAttribute('data-x', `${x}`);
+        box.setAttribute('data-y', `${y}`);
+        row.appendChild(box);
+      }
+
+      for (let i = 0; i < 10; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < 10; j++) {
+          createBox(row, j, i);
+        }
+        setUpGrid.appendChild(row);
+      }
+    }
+
+    // Add event listeners for leftside buttons
+    // Prob have to add data-ids to stuff
+
+    // Also make sure the stuff can get cleared
+
+    createGrid();
+  }
+
+  setUp();
+}
+
+ScreenController();
